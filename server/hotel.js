d270e0f7e5a54d6812b7edc561520f626e058f39
Elisyo <floguilbert@gmail.com>
2016-06-24 16:37:42 +0200

    Basic Chat in Node.js

---
+++ server/hotel.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
+
+var app = express();
+
+app.use(morgan('combined')) // Active le middleware de logging
+    .use(express.static(__dirname + '.')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+    .use(favicon(__dirname + '../img/favicon.ico')); // Active la favicon indiquée
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil, que puis-je pour vous ?');
+});
+
+app.get('/sous-sol', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+
+app.get('/etage/1/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hé ho, c\'est privé ici !');
+});
+
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.render('chambre.ejs', {etage: req.params.etagenum});
+});
+
+app.get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
+app.listen(8080);
